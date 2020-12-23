3ce0d3f65dce03679a9f8aa76994e357a1984d12
Berkay Özdemir <60180817+Ozdemir-B@users.noreply.github.com>
2020-12-23 17:40:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,26 @@
-from flask import Flask,request
-from flask import render_template
-from PIL import Image
-import numpy as np
+from flask import Flask , request , render_template
+#from PIL import Image
 from model import model
 
-app = Flask(__name__)
+app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "home"
 
-@app.route("/files")
-def files():
-    return render_template("pass.html")
-
-
-@app.route("/upload", methods=['GET','POST'])
+@app.route("/upload",methods=['GET','POST'])
 def upload():
     imagefile = request.files.get("imagefile")
-    arr = np.array(imagefile)
-    Image.open(imagefile).show()
-    """
-    modell = model("labels.txt","custom-model.txt")
-    labels = modell.labels
-    modell.execute(imagefile)
-    label = modell.labeled()"""
 
-    #print(type(labels),labels)
-    
+    modell = model()
+    labels=modell.labels
 
     if imagefile:
-        return "label"
-    else:
-        return "false"
+        return str(imagefile) + "\n\n*******\n\n" + str(labels)
 
+    else:
+        return "imagefile FALSE"
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run()
